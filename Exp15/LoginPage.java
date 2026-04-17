435589a60474a0153b2510c327236d2bc714ca97
YashrajSwamy <69750550+YashrajSwamy@users.noreply.github.com>
2026-04-17 19:44:03 +0530

    adding loginpage code

---
+++ Exp15/LoginPage.java
@@ -0,0 +1,68 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+
+class LPage extends JFrame
+{
+    JLabel userName;
+    JLabel password;
+    JTextField user;
+    JPasswordField pass;
+    JButton login;
+    JButton close;
+    LPage()
+    {
+        userName = new JLabel("Username: ");
+        userName.setBounds(200,200,100,30);
+        password = new JLabel("Password: ");
+        password.setBounds(200,250,100,30);
+
+        user = new JTextField();
+        user.setBounds(270,200,120,30);
+
+        pass = new JPasswordField();
+        pass.setBounds(270,250,120,30);
+
+        login = new JButton("Login");
+        login.setBounds(200,300,80,30);
+        login.addActionListener(e->{
+            if (user.getText().equals("a") && String.valueOf(pass.getPassword()).equals("a")) 
+            {
+                JOptionPane.showMessageDialog(this,"Login Successful\n"+"Welcome "+user.getText(),"Success",JOptionPane.INFORMATION_MESSAGE);
+                this.dispose();
+            }
+            else
+            {
+                JOptionPane.showMessageDialog(null,"Invalid Credentials","Invalid Credentials",JOptionPane.ERROR_MESSAGE);
+            }
+        });
+        
+        close = new JButton("Close");
+        close.setBounds(310,300,80,30);
+        close.addActionListener(e->{
+            this.dispose();
+        });
+        
+        this.add(userName);
+        this.add(user);
+        this.add(password);
+        this.add(pass);
+        this.add(login);
+        this.add(close);
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(null);
+        this.setSize(600,600);
+        this.setResizable(false);
+        this.setVisible(true);
+    }
+}
+
+public class LoginPage
+{
+    public static void main(String[] args)
+    {
+        LPage loginPage = new LPage();
+
+    }
+}
