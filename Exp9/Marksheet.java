e5a01301b1b33ac157a8fb5c3d8558f8d8fc30fc
YashrajSwamy <69750550+YashrajSwamy@users.noreply.github.com>
2026-04-17 19:25:26 +0530

    Add files via upload

---
+++ Exp9/Marksheet.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+class Student
+{
+    int rollNo;
+    Student(int rollNo)
+    {
+        this.rollNo = rollNo;
+    }
+    void display()
+    {
+        System.out.println("Roll No: "+rollNo);
+    }
+}
+
+class Test extends Student
+{
+    int Sem1_marks;
+    int Sem2_marks;
+    Test(int rollNo,int Sem1_marks,int Sem2_marks)
+    {
+        super(rollNo);
+        this.Sem1_marks = Sem1_marks;
+        this.Sem2_marks = Sem2_marks;
+    }
+    void display()
+    {
+        super.display();
+        System.out.println("Sem 1 Marks: "+Sem1_marks);
+        System.out.println("Sem 2 Marks: "+Sem2_marks);
+    }
+}
+
+
+class Result extends Test
+{
+    int total;
+    Result(int rollNo,int Sem1_marks,int Sem2_marks)
+    {
+        super(rollNo,Sem1_marks,Sem2_marks);
+    }
+    void display()
+    {
+        super.display();
+        total = Sem1_marks+Sem2_marks;
+        System.out.println("The Total Marks of 1st Year is : "+total);
+    }
+}
+public class Marksheet
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Roll No: ");
+        int rollNo = sc.nextInt();
+        System.out.print("Enter Sem 1 Marks: ");
+        int Sem1_marks = sc.nextInt();
+        System.out.print("Enter Sem 2 Marks: ");
+        int Sem2_marks = sc.nextInt();
+        Result r = new Result(rollNo,Sem1_marks,Sem2_marks);
+        r.display();
+    }
+}
